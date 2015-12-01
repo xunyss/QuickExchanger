5dc75a4e3dae2bfe50e996b24cf9f252e7262fca
xunyss <xunyss@gmail.com>
2015-12-01 13:09:21 +0900

    InetSetting 프로젝트 추가

---
+++ QuickExchanger/InetSetting/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace InetSetting
+{
+    static class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        const string REFRESH = "/refresh";
+
+
+        /// <summary>
+        /// 해당 응용 프로그램의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                processOption(REFRESH);
+            }
+            else
+            {
+                processOption(args[0]);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="option"></param>
+        static void processOption(string option)
+        {
+            switch (option)
+            {
+                case REFRESH:
+                    WinInetWrap.RefreshInternetSettings();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ QuickExchanger/InetSetting/WinInetWrap.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace InetSetting
+{
+    class WinInetWrap
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private const int INTERNET_OPTION_REFRESH = 37;
+        private const int INTERNET_OPTION_SETTINGS_CHANGED = 39;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="hInternet"></param>
+        /// <param name="dwOption"></param>
+        /// <param name="lpBuffer"></param>
+        /// <param name="lpdwBufferLength"></param>
+        /// <returns></returns>
+        [DllImport("wininet.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntPtr lpBuffer, int lpdwBufferLength);
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void RefreshInternetSettings()
+        {
+            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_SETTINGS_CHANGED, IntPtr.Zero, 0);
+            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_REFRESH, IntPtr.Zero, 0);
+        }
+    }
+}
